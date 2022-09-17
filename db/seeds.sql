e3c0123bdf23721897ec20022715effa60df1052
Jacob Canepa <jacobmcanepa@gmail.com>
2022-09-17 02:32:31 -0700

    added files & set up sql database

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES
+  ('Engineering'),
+  ('Finance'),
+  ('Legal'),
+  ('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Sales Lead', 100000, 4),
+  ('Salesperson', 80000, 4),
+  ('Lead Engineer', 150000, 1),
+  ('Software Engineer', 120000, 1),
+  ('Account Manager', 160000, 2),
+  ('Accountant', 125000, 2),
+  ('Legal Team Lead', 250000, 3),
+  ('Lawyer', 190000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+  ('John', 'Doe', 1),
+  ('Ed', 'Chambers', 2),
+  ('Ashly', 'Johnson', 3),
+  ('Kevin', 'Spice', 4),
+  ('Vandeep', 'Gunda', 5),
+  ('Malia', 'Brown', 6),
+  ('Mike', 'Mikeler', 7),
+  ('Saul', 'Goodman', 8);
